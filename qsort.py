49d5291e458ff4edc5e65799b34b5b506a5906e4
Ledenel <ledenelintelli@gmail.com>
2019-07-03 22:20:01 +0800

    add initial toy version.

---
+++ qsort.py
@@ -0,0 +1,22 @@
+import random
+
+def pick_pivot_index(array):
+    return 0
+
+def qsort(array):
+    if len(array) >= 1:
+        pivot_index = pick_pivot_index(array)
+        pivot = array[pivot_index]
+        remains = [x for i,x in enumerate(array) if i != pivot_index]
+        less_equal_than_pivot = [x for x in remains if x <= pivot]
+        greater_than_pivot = [x for x in remains if x > pivot]
+        return qsort(less_equal_than_pivot) + [pivot] + qsort(greater_than_pivot)
+    else:
+        return array
+
+if __name__ == "__main__":
+    unsorted = [1,2,3,4,5,6,7,8,9]
+    random.shuffle(unsorted)
+    print("unsorted: {0}".format(unsorted))
+    print("sorted: {0}".format(qsort(unsorted)))
+    
